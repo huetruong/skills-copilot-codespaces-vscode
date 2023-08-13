f2093fa0a6e6ea5d51fc583d0606af05a3e13178
Hue Truong <68408963+huetruong@users.noreply.github.com>
2023-08-13 16:17:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  if (member.style.display === "none") {
+    member.style.display = "block";
+  } else {
+    member.style.display = "none";
+  }
+}
